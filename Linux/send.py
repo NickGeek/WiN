10c4f528ef08766a205d561e3ee85a4c9b104770
Nick Webster <nick@nick.geek.nz>
2013-11-08 11:52:18 +1300

    FILL IN ALL THE DAMN FIELDS!

--- Linux/send.py
+++ Linux/send.py
@@ -6,13 +6,17 @@ sender = sender.rstrip()
 target = str(raw_input("Target's Username: "))
 message = str(raw_input("Message: "))
 
-#Messages are encoded like so "senderProgramVx.x##target##sender##message"
-#Example: "linuxV1.8##person87##NickGeek##Hey mate! What do you think of this WiN thing?"
-formattedMessage = "linuxVpre.release##"+target+"##"+sender+"##"+message
+#Make sure the message isn't blank
+if message != "" and sender != "" and target != "":
+	#Messages are encoded like so "senderProgramVx.x##target##sender##message"
+	#Example: "linuxV1.8##person87##NickGeek##Hey mate! What do you think of this WiN thing?"
+	formattedMessage = "linuxVpre.release##"+target+"##"+sender+"##"+message
 
-#Write to file
-messageFile = open('msg.txt', 'w+')
-messageFile.write(formattedMessage)
-messageFile.close()
+	#Write to file
+	messageFile = open('msg.txt', 'w+')
+	messageFile.write(formattedMessage)
+	messageFile.close()
 
-os.system("python server.py")
+	os.system("python server.py")
+else:
+	print "Error: Please fill out all the fields."
