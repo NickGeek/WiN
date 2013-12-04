69d67e58178cd02081950954880d9da2c888f5e6
Nick Webster <nick@nick.geek.nz>
2013-12-04 21:36:16 +1300

    Notification sounds on the Android app

--- Android/WiN/src/nz/net/win/MainActivity.java
+++ Android/WiN/src/nz/net/win/MainActivity.java
@@ -4,13 +4,17 @@
 package nz.net.win;
 
 import android.app.Activity;
+import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.os.PowerManager;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.TaskStackBuilder;
 import android.view.View;
@@ -184,6 +188,9 @@ public class MainActivity extends Activity {
 		    (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 		//I'm setting this to false for now because there is currently not a setup to display the message once the notification is closed
 		mBuilder.setAutoCancel(false);
+		Uri sound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+		mBuilder.setSound(sound);
+		mBuilder.setDefaults(Notification.DEFAULT_LIGHTS | Notification.DEFAULT_VIBRATE);
 		mNotificationManager.notify(1, mBuilder.build());
 	}
 }
